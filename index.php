d3d9b060e51d4182ae36a0186d946df0c4ceba60
mishraiit <ayushmishra.iit@gmail.com>
2014-05-15 13:49:42 +0530

    .

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
-	/* */	echo "Hello World";
+	
 ?>
+
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>
+				Mario Jumpak
+			</title>
+		
+			<style>
+				#canvas {
+					background-color:black;
+				}
+
+			</style>
+		</head>
+
+		<body>
+			 <canvas id="canvas" width="100%" height="100%"> </canvas>
+		
+			
+		</body>
+		<script>
+
+		</script>
+
+	</html>
+
