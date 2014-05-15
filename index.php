ec1904f47aec415a0ab3fae500d9eedb06a7f520
mishraiit <ayushmishra.iit@gmail.com>
2014-05-15 14:14:05 +0530

    .

--- index.php
+++ index.php
@@ -16,12 +16,21 @@
 
 				#leaderboard {
 					overflow:scroll;
+					border:3px double pink;
+				}
+
+				#header {
+					height:200px;
+					border:1px solid black;
 				}
 
 			</style>
 		</head>
 
 		<body>
+			<div id="header">
+
+			</div>
 			<table> 
 				<tr>
 					<td>
@@ -31,18 +40,23 @@
 						LeaderBoard
 					</td>
 				</tr>
+			</table>
 		
 			
 		</body>
 		<script>
 			var canvas = document.getElementById("canvas");
 			var context = document.getElementById("context");
+			var canvas_width = 800
+			var canvas_height = 600
 
-			canvas.setAttribute("width", 600+"px");
-			canvas.setAttribute("height",400+"px");
+			canvas.setAttribute("width", canvas_width+"px");
+			canvas.setAttribute("height",canvas_height+"px");
 
-			document.getElementById("leaderboard").setAttribute("width", (window.innerWidth -600) + "px");
-			document.getElementById("leaderboard").setAttribute("height", (window.innerHeight - 400) + "px");
+			document.getElementById("leaderboard").setAttribute("width", (window.innerWidth -canvas_width) + "px");
+			document.getElementById("leaderboard").setAttribute("height", (window.innerHeight - canvas_height) + "px");
 
+			document.getElementById("header").setAttribute("width", (window.innerWidth-30)+"px");
+			
 		</script>
 	</html>
